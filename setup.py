cbba2c66b2238f8a2cbb6203009e17f859d24cb4
Chris Antonellis <cantonelis@wayfair.com>
2021-01-12 14:57:24 -0500

    added project skeleton, support files, example code to prove test suide runs locally

---
+++ setup.py
@@ -0,0 +1,39 @@
+# type: ignore
+import ast
+import re
+
+import setuptools
+
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+with open("little_cheesemonger/__init__.py", "rb") as f:
+    _match = _version_re.search(f.read().decode("utf-8"))
+    if _match is None:
+        print("No version found")
+        raise SystemExit(1)
+    version = str(ast.literal_eval(_match.group(1)))
+
+
+setuptools.setup(
+    name="little-cheesemonger",
+    version=version,
+    url="https://github.com/wayfair-incubator/little-cheesemonger",
+    author="Chris Antonellis",
+    author_email="cantonellis@wayfair.com",
+    description="Modify an environment to build and bundle a Python package into a manylinux wheel.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    package_data={"little_cheesemonger": ["py.typed"]},
+    python_requires=">=3.6",
+    install_requires=["click~=7.1"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
---
+++ tests/test_example.py
@@ -0,0 +1,5 @@
+from little_cheesemonger import greeting
+
+
+def test_greeting():
+    assert greeting() == "hello, world"
