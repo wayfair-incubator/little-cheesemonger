2de369f7a4850c2aaa36da72647067dffbef37ba
Alexander Truslow <alexandertruslow@alexs-mac.home>
2021-02-02 07:38:34 -0500

    Adding explination of setup.py shim

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-
 #!/usr/bin/env python
+
+# We still need a setup.py shim so we can still pip install -e .
+# https://snarky.ca/what-the-heck-is-pyproject-toml/#how-to-use-pyproject-toml-with-setuptools
 
 import setuptools
 
