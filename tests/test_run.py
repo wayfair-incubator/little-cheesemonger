139f4869ee9ca4171175da75ae775f402e215f5b
Chris Antonellis <cantonelis@wayfair.com>
2021-02-02 10:44:19 -0500

    stashing test stubs

---
+++ tests/test_run.py
@@ -0,0 +1,25 @@
+
+
+
+def test_run__set_environment_variables_called_with_configuration():
+    pass
+
+
+def test_run__install_system_dependencies_called_with_configuration():
+    pass
+
+
+def test_run__install_python_dependencies_called_with_configuration():
+    pass
+
+
+def test_run__execute_commands_called_with_configuration():
+    pass
+
+
+def test_set_environment_variables__os_environ_update_called():
+    pass
+
+
+def test_set_environment_variables__raise_LittleCheesemongerError():
+    pass
