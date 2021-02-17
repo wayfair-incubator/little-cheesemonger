21ec60d0ba7e9a83e6092f2cdf23e8d3e9ebf28b
Chris Antonellis <cantonelis@wayfair.com>
2021-02-17 08:22:21 -0500

    making the linters happy

--- little_cheesemonger/__init__.py
+++ little_cheesemonger/__init__.py
@@ -1,5 +1,4 @@
-from little_cheesemonger._run import run  # noqa
 from little_cheesemonger._errors import LittleCheesemongerError  # noqa
-
+from little_cheesemonger._run import run  # noqa
 
 __version__ = "0.1.0rc0"
