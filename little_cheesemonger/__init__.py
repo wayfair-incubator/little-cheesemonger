91b67b4a74ead5ea15925fa2dda480a7667faa7c
Chris Antonellis <cantonelis@wayfair.com>
2021-02-18 08:46:13 -0500

    bump version in prep for another release candidate

--- little_cheesemonger/__init__.py
+++ little_cheesemonger/__init__.py
@@ -2,4 +2,4 @@ from little_cheesemonger._errors import LittleCheesemongerError  # noqa
 from little_cheesemonger._run import run  # noqa
 from little_cheesemonger._constants import Architecture, Platform, PythonVersion, PYTHON_BINARIES
 
-__version__ = "0.1.0rc1"
+__version__ = "0.1.0rc2"
