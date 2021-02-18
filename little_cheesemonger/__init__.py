c6e33d1e395b49a468da0fd34dfdcfbfbdf764d9
Chris Antonellis <cantonelis@wayfair.com>
2021-02-18 08:45:27 -0500

    expose constants for converting to PythonVersion syntax when using a custom data loader

--- little_cheesemonger/__init__.py
+++ little_cheesemonger/__init__.py
@@ -1,4 +1,5 @@
 from little_cheesemonger._errors import LittleCheesemongerError  # noqa
 from little_cheesemonger._run import run  # noqa
+from little_cheesemonger._constants import Architecture, Platform, PythonVersion, PYTHON_BINARIES
 
 __version__ = "0.1.0rc1"
